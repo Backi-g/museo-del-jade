3b961dc8a5156344d4cfa3157cc7a151bef5b182
Mariana Vargas <marvarga3@crsjopgp5744.global.publicisgroupe.net>
2018-04-24 19:37:21 -0600

    Mariana Feature El Recorrido version para integracion

--- src/js/ElRecorridoCtrl.js
+++ src/js/ElRecorridoCtrl.js
@@ -1,3 +1,31 @@
 angular.module('app').controller('ElRecorridoCtrl', [ '$scope', '$compile', function($scope, $compile) {
-    console.log('inside El Recorrido controller');
+    
+    $scope.h3_template="Bienvenido a El Recorrido";
+    $scope.p_template="Seleccione un nivel para conocer los detalles de las diferentes salas que ofrece el museo";
+    $scope.img_template = "src/images/elRecorrido.jpg";
+    $scope.style_thumbnailParagraph="font-family: 'Montserrat-Regular', sans-serif;font-style: normal;";
+    $scope.containerVisibility="visibility:visible;display:block;";
+
+    $scope.changeContent = function(planta) {
+		var img = "img_"+planta;
+		var h3="h3_"+planta;
+		var p="p_"+planta;
+
+		$scope.img_template = document.getElementById(img).src;
+		$scope.h3_template = document.getElementById(h3).textContent;
+		$scope.p_template = document.getElementById(p).textContent;
+		$scope.containerVisibility="visibility:visible;display:block;";
+	}
+	$scope.changeContentMobile=function(planta) {
+		var modalImage="modalImg_"+planta;
+		var h5="title_"+planta;
+		var modalParagraph="modalP_"+planta;
+
+	    $scope.modal_title=document.getElementById(h5).textContent;
+	    $scope.modal_img=document.getElementById(modalImage).src;
+	    $scope.modalVisibility="visibility:visible !important;display:block !important;";
+	    $scope.style_modalParagraph="font-family: 'Montserrat-Regular', sans-serif;font-style: normal;";
+	    $scope.modal_paragraph=document.getElementById(modalParagraph).textContent;
+
+	}
 }]);
